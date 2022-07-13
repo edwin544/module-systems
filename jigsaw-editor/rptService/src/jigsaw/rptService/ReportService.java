6e79594822c205fb140b3321d5541e2de0b0bfb2
Edwin Singh <edwin@Edwins-MBP.hub>
2022-07-14 10:16:01 +1200

    updated ReportService

--- jigsaw-editor/rptService/src/jigsaw/rptService/ReportService.java
+++ jigsaw-editor/rptService/src/jigsaw/rptService/ReportService.java
@@ -7,17 +7,16 @@ import jigsaw.loggerService.NetworkLogger;
 import java.io.IOException;
 
 public class ReportService {
+    private Logger _logger;
 
-    // private NetworkLogger _logger;
-   private Logger _logger;
     public ReportService(Logger logger) {
         _logger = logger;
     }
 
     public void submitReport(String title, String description) throws IOException {
         _logger.updateLog("Report Service, submitting report.");
-        var dataAccess = new ReportDataAccess(_logger);
-        dataAccess.updateReport(title, description);
+         var dataAccess = new ReportDataAccess(_logger);
+         dataAccess.updateReport(title, description);
     }
 
     public String getReport() throws IOException {
